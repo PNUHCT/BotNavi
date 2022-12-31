93fd699a10642ab925e8a766cc72abd1bcab5de6
Dongjin Kim <nyong9221@gmail.com>
2022-12-31 21:53:19 +0900

    app.config.js

---
+++ app.config.js
@@ -0,0 +1,9 @@
+module.exports = ({ config }) => ({
+  name: config.name,
+  slug: "SLUG_FROM_EAS",
+  extra: {
+    eas: {
+      projectId: "7cc8c45a-b351-4cd8-ad96-b77149886be1"
+    }
+  },
+});

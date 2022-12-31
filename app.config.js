f9745b3da396f56e82d610c87332fcba9e592c94
Dongjin Kim <nyong9221@gmail.com>
2022-12-31 23:56:36 +0900

    for Deploy

--- app.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config }) => ({
-  name: config.name,
-  slug: "SLUG_FROM_EAS",
-  extra: {
-    eas: {
-      projectId: "7cc8c45a-b351-4cd8-ad96-b77149886be1"
-    }
-  },
-});
